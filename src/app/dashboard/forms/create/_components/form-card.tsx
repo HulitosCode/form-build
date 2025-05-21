2a258e46e771957fbf9e8bc835b638f47fef9ffe
HulitosCode <heltonmatimbe8@gmail.com>
2025-05-21 13:44:45 +0200

    feat: adding form card and list. add filter to find or list individual form

---
+++ src/app/dashboard/forms/create/_components/form-card.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import Link from "next/link"
+
+type FormCardProps = {
+    id: string,
+    title: string,
+    description?: string | null,
+    responsesCount: number,
+    createdAt: Date
+}
+
+const FormCard = ({ id, title,description,responsesCount, createdAt }: FormCardProps) => {
+    const formattedDate = new Date(createdAt).toLocaleDateString()
+    return ( 
+        <Card className="h-full flex flex-col">
+            <CardHeader>
+                <CardTitle className="truncate">{title}</CardTitle>
+                {description && (
+                    <CardDescription className="line-clamp-2">
+                        {description}
+                    </CardDescription>
+                )}
+            </CardHeader>
+            <CardContent className="flex-1">
+                <p className="text-sm text-gray-500">{responsesCount} response</p>
+                <p className="text-sm text-gray-500">Created: {formattedDate}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between gap-2">
+                <Button asChild variant='outline' className="flex-1">
+                    <Link href={`/dashboard/forms/${id}`}>View</Link>
+                </Button>
+                 <Button asChild variant='outline' className="flex-1">
+                    <Link href={`/dashboard/forms/${id}/responses`}>Responses</Link>
+                </Button>
+                 <Button variant='destructive' className="flex-1">
+                    Delete
+                </Button>
+            </CardFooter>
+        </Card>
+     );
+}
+ 
+export default FormCard;
--- src/app/dashboard/forms/page.tsx
+++ src/app/dashboard/forms/page.tsx
@@ -1,9 +1,27 @@
+import prisma from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import FormList from "./create/_components/form-list";
 
 const Forms = async () => {
   const { userId, redirectToSignIn } = await auth();
 
   if (!userId) return redirectToSignIn();
+
+  const forms = await prisma.form.findMany({
+    where: {
+      userId
+    },
+    include: {
+      _count: {
+        select: {
+          responses: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return (
     <div className="space-y-6">
@@ -11,7 +29,7 @@ const Forms = async () => {
         <h1 className="text-3 font-bold">My Forms</h1>
         <p className="text-gray-500 mt-1">Create and manage your forms</p>
       </div>
-      {/* FormList */}
+      <FormList forms={forms} />
     </div>
   );
 };
